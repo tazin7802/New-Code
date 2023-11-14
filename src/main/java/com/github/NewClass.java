6ed35476169028aabc959100a42898d6328d831b
tazin7802 <tazin811@yahoo.com>
2023-11-13 19:35:21 -0600

    New Class

---
+++ src/main/java/com/github/NewClass.java
@@ -0,0 +1,8 @@
+package com.github;
+
+public class NewClass {
+	public static void main(String[] args) {
+		System.out.println("Second Class");
+	}
+
+}
--- src/main/java/com/github/PracticeGitHub.java
+++ src/main/java/com/github/PracticeGitHub.java
@@ -1,5 +1,14 @@
 package com.github;
 
 public class PracticeGitHub {
-
+	
+	public void getgit() {
+		
+	System.out.println("THis is practice run");
+	
+	}
+public static void main(String[] args) {
+	PracticeGitHub obj = new PracticeGitHub();
+	obj.getgit();
+}
 }
